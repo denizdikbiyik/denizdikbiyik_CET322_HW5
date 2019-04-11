51df6b9228e74b5e3a942c04317a3ff630e6549e
denizdikbiyik <denizdikbiyik@gmail.com>
2019-04-11 10:36:32 +0300

    denizdikbiyik_CET322_HW5
    denizdikbiyik_CET322_HW5

---
+++ denizdikbiyik_CET322_HW5/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace denizdikbiyik_CET322_HW5.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<denizdikbiyik_CET322_HW5.Models.Student> Student { get; set; }
+
+        public DbSet<denizdikbiyik_CET322_HW5.Models.Department> Department { get; set; }
+    }
+}
